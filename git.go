d1c4002d7ab03b38908ba044d3d082b2eeaaceea
Zichao LI <cszichao@gmail.com>
2023-12-15 10:33:52 +0800

    opt annotated tags

--- git.go
+++ git.go
@@ -20,10 +20,20 @@ func GetLatestVersion(repo *git.Repository) (*VerInfo, error) {
 	}
 	tagMap := make(map[plumbing.Hash]string)
 	if err := tagRefs.ForEach(func(t *plumbing.Reference) error {
-		if tObj, err := repo.TagObject(t.Hash()); err == nil {
-			tagMap[tObj.Target] = t.Name().Short()
-		} else {
-			tagMap[t.Hash()] = t.Name().Short()
+		tagMap[t.Hash()] = t.Name().Short()
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	// annotated tags
+	tagObjRefs, tagObjErr := repo.TagObjects()
+	if tagObjErr != nil {
+		return nil, tagObjErr
+	}
+	if err := tagObjRefs.ForEach(func(t *object.Tag) error {
+		if _, exists := tagMap[t.Hash]; exists { // deleted annotated tags may also have obj
+			tagMap[t.Target] = t.Name
 		}
 		return nil
 	}); err != nil {
